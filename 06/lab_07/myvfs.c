5e61e41c3c84e4d17526aa14f4da3653418cc689
fleshbound <117504017+fleshbound@users.noreply.github.com>
2024-05-05 13:17:32 +0300

    add slab

--- 06/lab_07/myvfs.c
+++ 06/lab_07/myvfs.c
@@ -7,12 +7,18 @@
 MODULE_LICENSE("GPL");
 
 #define myvfs_MAGIC_NUMBER 0xB16B00B5
+#define MAX_CACHE_SIZE 128
+#define SLAB_NAME "myvfs_cache"
+
+static struct kmem_cache *cache = NULL;
+static void **cache_mem = NULL;
+static int cached_count = 0;
 
 static struct myvfs_inode
 {
     int i_mode;
     unsigned long i_ino;
-} myvfs_inode;
+};
 
 static void myvfs_put_super(struct super_block *sb)
 {
@@ -25,9 +31,33 @@ static struct super_operations const myvfs_super_ops = {
     .drop_inode = generic_delete_inode,
 };
 
+static struct myvfs_inode *myvfs_cache_get_inode(void)
+{
+    printk(KERN_INFO "+ myvfs: call myvfs_cache_get_inode\n");
+
+    if (cached_count == MAX_CACHE_SIZE)
+    {
+        printk(KERN_ERR "+ myvfs: reached MAX_CACHE_SIZE\n");
+        return NULL;
+    }
+
+    void *ret = cache_mem[cached_count++] = kmem_cache_alloc(cache, GFP_KERNEL);
+
+    if (!ret)
+    {
+        printk(KERN_ERR "+ myvfs: can't kmem_cache_alloc\n");
+        kmem_cache_free(cache, *cache_mem);
+        kmem_cache_destroy(cache);
+        kfree(cache_mem);
+    }
+
+    return ret;
+}
+
 static struct inode *myvfs_make_inode(struct super_block *sb, int mode)
 {
     struct inode *ret = new_inode(sb);
+    struct myvfs_inode *cache_inode = NULL;
     
     if (ret)
     {
@@ -38,7 +68,17 @@ static struct inode *myvfs_make_inode(struct super_block *sb, int mode)
 
         ret->i_mode = mode;
         ret->i_atime = ret->i_mtime = ret->i_ctime = current_time(ret);
-        ret->i_private = &myvfs_inode;
+
+        if (NULL == (cache_inode = myvfs_cache_get_inode()))
+        {
+            iput(ret);
+        }
+        else
+        {
+            cache_inode->i_mode = ret->i_mode;
+            cache_inode->i_ino = ret->i_ino;
+            ret->i_private = cache_inode;
+        }
     }
 
     return ret;
@@ -102,6 +142,11 @@ static struct file_system_type myvfs_type = {
     .fs_flags = FS_USERNS_MOUNT,
 };
 
+void f_init(void *p)
+{
+    *(int *)p = (int) p;
+}
+
 static int __init myvfs_init(void)
 {
     int ret = register_filesystem(&myvfs_type);
@@ -112,6 +157,31 @@ static int __init myvfs_init(void)
         return ret;
     }
 
+    if (NULL == (cache_mem = kmalloc(sizeof(struct myvfs_inode *) * MAX_CACHE_SIZE, GFP_KERNEL)))
+    {
+        printk(KERN_ERR "+ myvfs: can't kmalloc cache\n");
+        return -ENOMEM;
+    }
+
+    if (NULL == (cache = kmem_cache_create(SLAB_NAME, sizeof(struct myvfs_inode), 0, SLAB_HWCACHE_ALIGN, f_init)))
+    {
+        printk(KERN_ERR "+ myvfs: can't kmem_cache_create\n");
+        kmem_cache_destroy(cache);
+        kfree(cache_mem);
+        return -ENOMEM;
+    }
+
+    /*if (NULL == ((*cache_mem) = kmem_cache_alloc(cache, GFP_KERNEL)))
+    {
+        printk(KERN_ERR "+ myvfs: can't kmem_cache_alloc\n");
+        kmem_cache_free(cache, *cache_mem);
+        kmem_cache_destroy(cache);
+        kfree(cache_mem);
+        return -ENOMEM;
+    }*/
+
+    printk(KERN_INFO "+ myvfs: alloc %d objects into slab: %s\n", cached_count, SLAB_NAME);
+    printk(KERN_INFO "+ myvfs: object size %ld bytes, full size %ld bytes\n", sizeof(struct myvfs_inode), sizeof(struct myvfs_inode *) * MAX_CACHE_SIZE);
     printk(KERN_INFO "+ myvfs: module loaded\n");
 
     return 0;
@@ -123,6 +193,12 @@ static void __exit myvfs_exit(void)
 
     if (ret)
         printk(KERN_ERR "+ myvfs: can't unregister_filesystem\n");
+
+    for (size_t i = 0; i < cached_count; i++)
+        kmem_cache_free(cache, cache_mem[i]);
+
+    kmem_cache_destroy(cache);
+    kfree(cache_mem);
 
     printk(KERN_INFO "+ myvfs: module unloaded\n");
 }
